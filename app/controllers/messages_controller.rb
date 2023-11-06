a6fe15e221fc128fd9829f0735b2be05c5a7b4cd
W3villa <vishalprakash.meaurya@w3villa.com>
2023-11-06 18:40:49 +0530

    cable error

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,23 +1,27 @@
 class MessagesController < ApplicationController
-    before_action :require_user,only:[:create,:index]
-    def index
-        @messages = Message.all
+  before_action :require_user, only: [:create, :index]
+
+  def index
+    @messages = Message.all
+  end
+
+  def create
+    @message = current_user.messages.new(message_params)
+    if @message.save 
+      ActionCable.server.broadcast 'chatroom_channel', message: @message.body
+    else
+      flash[:alert] = "message did not saved"
+      redirect_to root_path
     end
-    def create
-         @message = current_user.message.build(params.require(:message).permit(:body))
-         if @message.save
-            # ActionCable.server.broadcast 'chatroom_channel',message: message_render(@message)
-         else
-            flash[:alert]= "message did not saved"
-            redirect_to root_path
-         end
-    end
+  end
 
-    private
+  private
 
-    def message_render(message)
+  def message_params
+    params.require(:message).permit(:body)
+  end
 
-      render(partial: "message",locals:{message: message} )
-    end
-
+  def message_render(message)
+    render(partial: "message", locals: { message: message })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  root 'chatroom#index'
+  root "chatroom#index"
 
-  resources :users,only:[:new,:create]
+  resources :users, only: [:new, :create]
 
-  get 'login', to: 'session#new'
-  post 'login', to: 'session#create'
-  delete 'logout', to: 'session#destroy'
-  post 'message', to: 'message#create'
+  get "login", to: "session#new"
+  post "login", to: "session#create"
+  delete "logout", to: "session#destroy"
+  post "messages", to: "messages#create"
+
+  mount ActionCable.server, at: "/cable"
 end
